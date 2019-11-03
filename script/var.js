1753c42b5d6860adc7327be8a1142ece1d72130f
rafaelds9 <duarte.rafael@academico.ifpb.edu.br>
2019-11-03 03:25:08 -0300

    Updated to my settings

--- script/var.js
+++ script/var.js
@@ -1,21 +1,21 @@
 // All the variable for the app
 // Their name are explicit
 
-var username = "Cel51"
+var username = "Rafael"
 var clientid = "be212a58528168962a39c64052c1d88e";
 var redirecturi = "http://localhost:8002/callback.html";
 var locations = [
   "784201",
-  "784302"
+  "784302",
 ];
 var images = [
   "1412446496031.jpg",
   "1417804954510.jpg",
-  "1422771896804.jpg",
+  //"1422771896804.jpg",
   "1424655132831.jpg",
-  "1425939966695.jpg",
+  //"1425939966695.jpg",
   "1429482830109.jpg",
-  "1429569823779.jpg",
+  //"1429569823779.jpg",
   "1437167260211.jpg",
   "1437214448937.jpg",
   "1437214448937.jpg",
@@ -24,7 +24,7 @@ var images = [
   "1444827771939.jpg",
   "1445223016379.jpg",
   "1445223050369.jpg",
-  "1445370353275.jpg",
+  //"1445370353275.jpg",
   "1445705808951.jpg",
   "1445713184723.jpg",
   "1448399280112.jpg",
@@ -32,40 +32,40 @@ var images = [
   "1450074394745.jpg"
 ];
 var searchs = [
-  ["!g", "https://www.google.ch/#q="],
+  ["!g", "https://www.google.com/#q="],
   ["!y", "https://www.youtube.com/results?search_query="],
-  ["!w", "https://en.wikipedia.org/w/index.php?search="]
+  ["!w", "https://en.wikipedia.org/w/index.php?search="],
+  ["!r", "https://www.reddit.com/search/?q="],
+  ["!d", "https://en.wiktionary.org/w/index.php?search="],
+  ["!s", "https://scholar.google.com.br/scholar?lr=&q="]
 ];
 var favorites = [
   [ "Work",
     [
-      ["cpnv" , "http://intranet.cpnv.ch/", "cpnv"],
-      ["github" , "https://github.com/", "gh"],
-      ["gmail" , "https://mail.google.com/mail/u/0/#inbox", "gm"],
-      ["bitbucket" , "http://bitbucket.org", "bb"]
+      ["Gmail" , "https://mail.google.com/mail/u/0/#inbox", "gm"],
+      ["Github" , "https://github.com/", "gh"],
+      ["ResearchGate" , "https://www.researchgate.net/", "rg"]
     ]
   ],
   [ "Social",
     [
-      ["whatsapp" , "https://web.whatsapp.com/", "wa"],
-      ["hangouts" , "http://hangouts.google.com", "hang"],
-      ["facebook" , "https://www.facebook.com/" , "fb"],
-      ["twitter" , "https://twitter.com/", "twi"]
+      ["Whatsapp" , "https://web.whatsapp.com/", "wa"],
+      ["Facebook" , "https://www.facebook.com/" , "fb"],
+      ["Twitter" , "https://twitter.com/", "twi"],
+      ["Hangouts" , "http://hangouts.google.com", "hang"]
     ]
   ],
   [ "Download",
     [
-      ["thepiratebay", "http://thepiratebay.se", "tpb"],
-      ["T411", "http://www.t411.in/", "t411"]
+      ["thepiratebay", "http://thepiratebay.org", "tpb"],
+      ["1337x", "https://1337x.to/", "1337x"]
     ]
   ],
   [ "Reddit",
     [
       ["Frontpage", "https://www.reddit.com/", "fp"],
-      ["LoL", "https://www.reddit.com/r/leagueoflegends/", "lol"],
+      ["Brasil", "https://www.reddit.com/r/brasil/", "br"],
       ["4chan", "https://www.reddit.com/r/4chan/", "4ch"],
-      ["Monster Hunter", "https://www.reddit.com/r/MonsterHunter/", "mh"],
-      ["Programmer Humor", "https://www.reddit.com/r/ProgrammerHumor/", "ph"]
     ]
   ],
   [ "4chan",
@@ -77,13 +77,13 @@ var favorites = [
   ],
   [ "Others",
     [
-      ["hugelol", "http://hugelol.com", "hgl"],
-      ["hiddenlol", "http://hiddenlol.com", "hdl"]
+      ["Google News", "http://news.google.com/", "news"]
     ]
   ]
 ];
+
 var feeds = [
-  ["20min Hi-Tech","http://www.20min.ch/rss/rss.tmpl?type=channel&get=20&lang=ro"],
-  ["Internet is Beautiful", "https://www.reddit.com/r/InternetIsBeautiful/.rss"],
-  ["Listen to this", "https://www.reddit.com/r/listentothis/.rss"]
+  //["20min Hi-Tech","http://www.20min.ch/rss/rss.tmpl?type=channel&get=20&lang=ro"],
+  //["Internet is Beautiful", "https://www.reddit.com/r/InternetIsBeautiful/.rss"],
+  //["Listen to this", "https://www.reddit.com/r/listentothis/.rss"]
 ];
